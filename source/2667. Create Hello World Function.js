a502fb6ac912430ff8e727bfa6052b257ae0c5e1
Aahan Salecha <salecha.aahan@gmail.com>
2025-03-26 17:45:28 +0530

    Create 2667. Create Hello World Function.js

---
+++ source/2667. Create Hello World Function.js
@@ -0,0 +1,11 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+// https://leetcode.com/problems/create-hello-world-function/solutions/6581876/solution-for-create-hello-world-function-hx5o/
