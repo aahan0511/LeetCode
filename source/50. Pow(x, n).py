d068dd4131c909496f35ec45d2795747fcde61df
Aahan Salecha <salecha.aahan@gmail.com>
2025-03-28 17:01:57 +0530

    Create 50. Pow(x, n).py

---
+++ source/50. Pow(x, n).py
@@ -0,0 +1,5 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+    
+# https://leetcode.com/problems/powx-n/solutions/6589490/solution-for-powx-n-in-python-by-aahan05-ynzr/
