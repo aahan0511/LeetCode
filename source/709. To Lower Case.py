85267903cb71907fc3b89e2a5470f90d98503f92
Aahan Salecha <salecha.aahan@gmail.com>
2025-03-28 21:24:08 +0530

    Create 709. To Lower Case.py

---
+++ source/709. To Lower Case.py
@@ -0,0 +1,5 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+# https://leetcode.com/problems/to-lower-case/solutions/6590174/solution-for-to-lower-case-in-python/
