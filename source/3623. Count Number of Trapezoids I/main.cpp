f24a87f5ba46a6187a3dead6aaf723b969e4db7b
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-02 17:03:00 +0530

    Add Solution for Daily 2025-12-02 | 3623

---
+++ source/3623. Count Number of Trapezoids I/main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<int, int> same_x;
+        
+        for (auto& point : points) {
+            same_x[point[1]]++;
+        }
+
+        long sum = 0;
+        long sqsum = 0;
+        for (pair<int, int> x : same_x) {
+            long n = x.second;
+            long pos = ((n - 1) * n) / 2;
+            sum += pos;
+            sqsum += pos * pos;
+        }
+
+        return (((sum * sum) - sqsum) / 2) % 1000000007;
+    }
+};
