3342247e7d05474e88e4b5bb143a3fc2dedff1b0
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-14 11:47:20 +0530

    Add Solution for "Daily 14 December 2025" (2147. Number of Ways to Divide a Long Corridor)

---
+++ source/Daily/2025-12-14.cpp
@@ -0,0 +1,35 @@
+// 2147. Number of Ways to Divide a Long Corridor
+
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        int c = count(corridor.begin(), corridor.end(), 'S');
+        if (c % 2 == 1 || c == 0) 
+            return 0;
+        
+        long long ways = 1;
+        
+        bool seats = false;
+        int last_seat = -1;
+        for (int i = 0; i < corridor.size(); i++) {
+            char &c = corridor[i];
+            
+            if (c == 'S') {
+                if (seats) {
+                    last_seat = i;
+                    seats = false;
+                } else {
+                    seats = true;
+                    if (last_seat != -1) 
+                        ways = (ways * (i - last_seat)) % 1000000007;
+                }
+            }
+        }
+        
+        return ways;
+    }
+};
