a52a5874724340affeb91163b46b7fd5ddc4b83c
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-15 11:49:47 +0530

    Add Solution for "459. Repeated Substring Pattern" in C++

---
+++ source/0459. Repeated Substring Pattern/main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int l = s.size();
+        
+        for (int n = 1; n < l; n++) {
+            if (l % n != 0) continue;
+            
+            const int x = l / n;
+            string t = "";
+            const string &u = s.substr(0, n);
+            
+            for (int i = 0; i < x; i++) {
+                t += u;
+            }
+            if (t == s) return true;
+        }
+        return false;
+    }
+};
