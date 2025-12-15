7a5bcef48ee37ff8037c329269e26312180486f8
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-15 16:52:04 +0530

    Add Solution for "682. Baseball Game" in C++

---
+++ source/0682. Baseball Game/main.cpp
@@ -0,0 +1,27 @@
+#include <numeric>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> scores;
+        for (string op : operations) {
+            int s = scores.size() - 1;
+            if (op == "+") {
+                int score = scores[s] + scores[s - 1];
+                scores.push_back(score);
+            } else if (op == "D") {
+                int score = scores[s] * 2;                
+                scores.push_back(score);
+            } else if (op == "C") {
+                scores.pop_back();
+            } else {
+                scores.push_back(stoi(op));
+            }
+        }
+        
+        return accumulate(scores.begin(), scores.end(), 0);
+    }
+};
