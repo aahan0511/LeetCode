2bbf89e06fac590318bbd76c9db91d97f7d64ec7
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-16 16:21:20 +0530

    Add Solution for "1137. N-th Tribonacci Number" in C++

---
+++ source/1137. N-th Tribonacci Number/main.cpp
@@ -0,0 +1,17 @@
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+    unordered_map<int, int> cache = {
+        {0, 0},
+        {1, 1},
+        {2, 1},
+        {3, 2},
+        {4, 4},
+    };
+    
+public:
+    int tribonacci(int n) {
+        return cache.count(n) ? cache[n] : (cache[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3));
+    }
+};
