7d7036b518322805c1ce331d9c2dbffc3d8653a3
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-16 16:15:41 +0530

    Add Solution for "509. Fibonacci Number" in C++

---
+++ source/Study Plan/Dynamic Programming/Fibonacci style/02. Fibonacci Number.cpp
@@ -0,0 +1,19 @@
+// 509. Fibonacci Number
+
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+unordered_map<int, int> cache = {
+    {0, 0},
+    {1, 1},
+    {2, 1},
+    {3, 2},
+    {4, 3},
+}; // THIS CAN BE ENLARGED AS BIG AS WANTED; BUT THIS SEEMS ENOUGH
+    
+public:
+    int fib(int n) {
+        return cache.count(n) ? cache[n] : (cache[n] = fib(n - 1) + fib(n - 2));
+    }
+};
