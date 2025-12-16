f0adb550d98746cbe4bb3b089dc3c51efba2dd0c
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-16 16:07:00 +0530

    Add Solution for "70. Climbing Stairs" to 'Dynamic Programming'

---
+++ source/Study Plan/Dynamic Programming/Fibonacci style/01. Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+// 70. Climbing Stairs
+
+#include <map>
+using namespace std;
+
+class Solution {
+map<int, int> cache;
+    
+public:
+    int climbStairs(int n) {
+        if (n < 3) return n;
+        if (cache.find(n) != cache.end())
+             return cache[n];
+        int ans = climbStairs(n - 1) + climbStairs(n - 2);
+        cache[n] = ans;
+        return ans;
+    }
+};
