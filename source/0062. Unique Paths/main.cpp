b476fe44108fad23b1c83e8713b79ffb5a3467b7
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-16 20:39:57 +0530

    Add Solution for "62. Unique Paths" in C++

---
+++ source/0062. Unique Paths/main.cpp
@@ -0,0 +1,15 @@
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+    unordered_map<long long, int> cache;
+    
+public:
+    int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) return 1;
+        if (m == 2 || n == 2) return max(m, n);
+        
+        long long key = max(m, n) * 1000 + min(m, n);
+        return cache.count(key) ? cache[key] : cache[key] = (uniquePaths(m - 1, n) + uniquePaths(m, n -1));
+    }
+};
