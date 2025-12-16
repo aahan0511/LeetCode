5c4b5453ce0ae8541dae1182833c5b72eed5e0c1
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-16 10:17:17 +0530

    Add Solution for "73. Set Matrix Zeroes" in C++

---
+++ source/0073. Set Matrix Zeroes/main.cpp
@@ -0,0 +1,47 @@
+#include <set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+int m, n;
+set<int> completed_rows;
+set<int> completed_cols;
+    
+void convert(vector<vector<int>>& matrix, pair<int, int>& pos) {
+    int row = pos.first;
+    int col = pos.second;
+    
+    if (completed_rows.find(row) == completed_rows.end()) {
+        for (int r = 0; r < n; ++r) {
+            matrix[row][r] = 0;
+        }
+        completed_rows.insert(row);
+    }
+    
+    if (completed_cols.find(col) == completed_cols.end()) {
+        for (int c = 0; c < m; ++c) {
+            matrix[c][col] = 0;
+        }
+        completed_cols.insert(col);
+    }
+}
+    
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        m = matrix.size();
+        n = matrix[0].size();
+        
+        vector<pair<int, int>> positions;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) 
+                    positions.push_back({i, j});
+            }
+        }
+        
+        for (pair<int, int> pos : positions)
+            convert(matrix, pos);
+    }
+};
