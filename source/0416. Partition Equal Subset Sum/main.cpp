2494727ecedd1ad5162611de6fcae201ab738def
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2025-12-22 15:14:54 +0530

    Add Solution for "416. Partition Equal Subset Sum" in C++

---
+++ source/0416. Partition Equal Subset Sum/main.cpp
@@ -0,0 +1,22 @@
+#include <numeric>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0); 
+        if (sum % 2 == 1) return false;
+        
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        
+        for (int num : nums) {
+            for (int s = target; s >= num; --s)
+                if (dp[s - num]) 
+                    dp[s] = true;
+        }
+        return dp[target];
+    }
+};
