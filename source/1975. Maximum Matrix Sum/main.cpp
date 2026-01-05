d249839aa1b1ebf8e4cadfa0ec53fc6f4d289094
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2026-01-05 16:15:32 +0530

    Add Solution for Daily 5th January 2026

---
+++ source/1975. Maximum Matrix Sum/main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        bool count = false;
+        int min_neg = INT_MAX;
+        long long sum = 0;
+        
+        for (vector<int> row : matrix) 
+            for (int cell : row)  {
+                if (cell < 0) {
+                    count = !count;
+                    sum -= cell;   
+                } else {
+                    sum += cell;
+                }
+                min_neg = min(min_neg, abs(cell));
+            }
+                
+        return sum - 2 * (count ? min_neg : 0);
+    }
+};
