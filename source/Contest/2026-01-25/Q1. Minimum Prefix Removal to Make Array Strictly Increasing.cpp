f05e00b4e7bbd569cd5aeaa407f336a27969b557
Aahan Salecha <188063520+aahan0511@users.noreply.github.com>
2026-01-25 09:41:33 +0530

    Add Solutions for Contest on 25th January 2026

---
+++ source/Contest/2026-01-25/Q1. Minimum Prefix Removal to Make Array Strictly Increasing.cpp
@@ -0,0 +1,16 @@
+// 3818. Minimum Prefix Removal to Make Array Strictly Increasing
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] >= nums[i+1]) 
+                return i + 1;
+        }
+        return 0;
+    }
+};
---
+++ source/Contest/2026-01-25/Q2. Rotate Non Negative Elements.cpp
@@ -0,0 +1,41 @@
+// 3819. Rotate Non Negative Elements
+
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+vector<int> rotate(vector<int> &nums, int k, int n) {
+    if (n == 0) return nums;
+    k = k % n;
+
+    vector<int> first_k(nums.begin(), nums.begin() + k);
+    vector<int> last_k(nums.begin() + k, nums.end());
+    last_k.insert(last_k.end(), first_k.begin(), first_k.end());
+
+    return last_k;
+}
+
+public:
+    vector<int> rotateElements(vector<int>& nums, int k) {
+        vector<int> non_negative;
+        vector<pair<int, int>> negative;
+        for (int num : nums) {
+            if (num < 0) {
+                negative.push_back({
+                    non_negative.size() + negative.size(),
+                    num
+                });
+            } else {
+                non_negative.push_back(num);
+            }
+        }
+
+        vector<int> rotated_non_negative = rotate(non_negative, k, non_negative.size());
+        for (pair<int, int> &neg : negative) {
+            rotated_non_negative.insert(rotated_non_negative.begin() + neg.first, neg.second);
+        }
+
+        return rotated_non_negative;
+    }
+};
